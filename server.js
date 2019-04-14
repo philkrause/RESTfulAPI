75fc7dcab8d3205eb4ea77d12cd24da2f8037a9b
sirdjango <38673429+sirdjango@users.noreply.github.com>
2019-04-13 20:55:52 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -57,6 +57,61 @@ router.route('/bears')
 
 })
 
+//get all the bears( accessed at GET localhost:8080/api/bears)
+.get(function(req,res){
+    Bear.find(function(err, bears){
+        if(err)
+            res.send(err);
+            res.json(bears);
+    });
+});
+
+//on routes that end in /bears/:bear_id
+//===============================================
+
+router.route('/bears/:bear_id')
+ 
+// get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)  
+.get(function(req,res){
+    Bear.findById(req.params.bear_id, function(err, bear){
+        if(err)
+            res.send(err)
+        res.json(bear);
+    });
+})
+
+// update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
+    .put(function(req,res){
+
+         // use our bear model to find the bear we want
+        Bear.findById(req.params.bear_id, function(err,bear){
+            if(err)
+            res.send(err)
+  
+           
+            bear.name = req.body.name; //update bears info
+
+           
+            //save the bear
+           bear.save(function(err){
+               if(err)
+                res.send(err);
+
+                res.json({message: 'Bear Updated'});
+           });
+        });
+    })
+ 
+//delete the bear with this id( accessed at DELETE http://localhost:8080/api/bears/:bears_id)
+.delete(function(req,res){
+    Bear.remove({_id: req.params.bear_id}, function(err,bears){
+        if(err)
+        res.send(err)
+        res.json({message: 'Bear Deleted'});
+    
+    });
+})
+
 
 
 
@@ -74,7 +129,7 @@ console.log('Magic Happens on port ' + port);
 // ============================================
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://user:<password>@cluster0-45nfb.mongodb.net/test')
+mongoose.connect('mongodb+srv://user:pillow123@cluster0-45nfb.mongodb.net/test')
 
 var Bear = require('./app/models/bear');
 
